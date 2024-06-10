ac9d1b568807d7fe923adf2f8e745a7f436c5378
georgievdd <akaden1017@mail.ru>
2024-06-10 19:03:01 +0300

    d

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const morgan = require('morgan')
+const app = express()
+app.use((req, res, next) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
+    console.log(`Client IP: ${clientIp}`);
+    next();
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :remote-addr :user-agent'))
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(8000, () => {
+    console.log("server started")
+});
